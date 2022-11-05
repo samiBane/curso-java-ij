0572aa08ecba1b5543aa0fdec89cd3c8684234be
samiBane <s.evelynalves@gmail.com>
2022-11-05 01:31:40 -0300

    primeiro commit

---
+++ src/br/com/dio/PrimeiroProjeto.java
@@ -0,0 +1,10 @@
+package br.com.dio;
+
+import br.com.dio.Gato.Gato;
+public class PrimeiroProjeto {
+    public static void main(String[] args) {
+//        System.out.println("Hello World");
+        Gato gato = new Gato();
+        System.out.println(gato);
+    }
+}
